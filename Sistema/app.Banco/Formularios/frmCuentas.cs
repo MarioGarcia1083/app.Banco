0e39a17b80ae56e2226d11bcd8e665ef1d8750c3
Mario García <mrgmairena@gmail.com>
2025-09-28 08:16:58 -0600

    Formulario Cuentas

--- Sistema/app.Banco/Formularios/frmCuentas.cs
+++ Sistema/app.Banco/Formularios/frmCuentas.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using app.Banco.Utilidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +18,61 @@ namespace app.Banco.Formularios
         {
             InitializeComponent();
         }
+
+        #region Métodos
+
+        private void ListarRegistros()
+        {
+            try
+            {
+                string connetionString = ConexionDB.ObtenerConexion();
+
+                using (SqlConnection conexion = new SqlConnection(connetionString))
+                {
+                    string consulta = @"
+                        SELECT
+                            idCuenta AS id,
+                            idCliente AS Cliente,
+                            idUsuario AS Usuario,
+                            saldoInicial AS [Saldo Inicial],
+                            saldoActual AS [Saldo Actual],
+                            estadoCuenta AS Estado
+                        FROM cuenta";
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(consulta, conexion);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    dgvListado.DataSource = dt;
+                    dgvListado.Columns[0].Visible = false;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region Eventos del Formulario
+
+        private void frmCuentas_Load(object sender, EventArgs e)
+        {
+            ListarRegistros();
+        }
+
+
+        #endregion
+
+        #region Botones de Comando
+
+        private void iconCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        #endregion
     }
 }
